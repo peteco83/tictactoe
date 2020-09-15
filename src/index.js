0a95b02a58a0a6fa9dbeb2adee515e2b0da58cc4
federico <peteco1983@outlook.com>
2020-09-15 11:18:12 +0200

    to deploy

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Game from "./components/Game.js";
+import reactDOM from "react-dom";
+import "./styles/index.css";
+
+reactDOM.render(<Game />, document.getElementById("root"));
