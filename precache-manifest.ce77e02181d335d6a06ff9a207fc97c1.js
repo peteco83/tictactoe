8f11898f348ddbd2b4956c058bc91f1d204966f4
federico <peteco1983@outlook.com>
2020-09-15 11:19:49 +0200

    Updates

---
+++ precache-manifest.ce77e02181d335d6a06ff9a207fc97c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001ae6b3c65128748efa96e48910e54a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "666b071018ad2f298ed9",
+    "url": "/static/css/main.511343c6.chunk.css"
+  },
+  {
+    "revision": "29407a214c356ae0e389",
+    "url": "/static/js/2.ab314cdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab314cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "666b071018ad2f298ed9",
+    "url": "/static/js/main.efdc04d5.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  }
+]);
